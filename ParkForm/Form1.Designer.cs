b8132a9226f54c22fff7ca98fc636ca8f8bae577
aleyna baltacı <aleynabaltaci@outlook.com>
2022-04-25 17:16:06 +0300

    Add project files.

---
+++ ParkForm/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace ParkForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Toplama = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Çıkarma = new System.Windows.Forms.Button();
+            this.ÜsAlma = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Toplama
+            // 
+            this.Toplama.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Toplama.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.Toplama.Location = new System.Drawing.Point(15, 181);
+            this.Toplama.Name = "Toplama";
+            this.Toplama.Size = new System.Drawing.Size(109, 32);
+            this.Toplama.TabIndex = 0;
+            this.Toplama.Text = "+";
+            this.Toplama.UseVisualStyleBackColor = true;
+            this.Toplama.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1.Sayı";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2.Sayı";
+            // 
+            // Çıkarma
+            // 
+            this.Çıkarma.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Çıkarma.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.Çıkarma.Location = new System.Drawing.Point(15, 231);
+            this.Çıkarma.Name = "Çıkarma";
+            this.Çıkarma.Size = new System.Drawing.Size(109, 32);
+            this.Çıkarma.TabIndex = 0;
+            this.Çıkarma.Text = "-";
+            this.Çıkarma.UseVisualStyleBackColor = true;
+            this.Çıkarma.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ÜsAlma
+            // 
+            this.ÜsAlma.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ÜsAlma.ForeColor = System.Drawing.Color.DarkGray;
+            this.ÜsAlma.Location = new System.Drawing.Point(15, 281);
+            this.ÜsAlma.Name = "ÜsAlma";
+            this.ÜsAlma.Size = new System.Drawing.Size(109, 32);
+            this.ÜsAlma.TabIndex = 4;
+            this.ÜsAlma.Text = "x^2";
+            this.ÜsAlma.UseVisualStyleBackColor = true;
+            this.ÜsAlma.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.button4.Location = new System.Drawing.Point(146, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "π";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.ForeColor = System.Drawing.Color.DarkCyan;
+            this.button5.Location = new System.Drawing.Point(146, 231);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 32);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "x!";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button6.Location = new System.Drawing.Point(146, 281);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 31);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "sinüs";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.ForeColor = System.Drawing.Color.Coral;
+            this.button7.Location = new System.Drawing.Point(279, 181);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(109, 32);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "kosinüs";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.ForeColor = System.Drawing.Color.Crimson;
+            this.button8.Location = new System.Drawing.Point(279, 231);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(109, 32);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "tanjant";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button9.Location = new System.Drawing.Point(279, 281);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(109, 30);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "kotanjant";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button10.ForeColor = System.Drawing.Color.DarkRed;
+            this.button10.Location = new System.Drawing.Point(15, 332);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(109, 32);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "radyan";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button11.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.button11.Location = new System.Drawing.Point(146, 332);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(109, 32);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "gradyan";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button12.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.button12.Location = new System.Drawing.Point(279, 332);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(109, 32);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "|x|";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.ÜsAlma);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Çıkarma);
+            this.Controls.Add(this.Toplama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Toplama;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Çıkarma;
+        private System.Windows.Forms.Button ÜsAlma;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ ParkForm/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParkForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox3.Text);
+            MessageBox.Show((a + b).ToString());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox3.Text);
+            MessageBox.Show((a - b).ToString());
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            MessageBox.Show((a*a).ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            decimal pi= (decimal)Math.PI;
+            MessageBox.Show(pi.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int sayı1= Convert.ToInt32(textBox1.Text);
+            int sonuc = 1;
+            for (int i =1; i<=sayı1 ; i++)
+            {
+                sonuc *= i;
+            }
+            MessageBox.Show(sonuc.ToString());
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int a= Convert.ToInt32(textBox1.Text);
+            MessageBox.Show((Math.Sin(a)).ToString());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            MessageBox.Show((Math.Cos(a)).ToString());
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            MessageBox.Show((Math.Tan(a)).ToString());
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            MessageBox.Show((1/Math.Tan(a)).ToString());
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            decimal pi= (decimal)Math.PI;
+            decimal sonuc = (a * pi)/180;
+            MessageBox.Show(sonuc.ToString());
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            decimal pi = (decimal)Math.PI;
+            decimal gradyan = Convert.ToDecimal((a * 200 ))/ pi;
+            MessageBox.Show(gradyan.ToString());
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int sonuc=Math.Abs(a);
+            MessageBox.Show(sonuc.ToString());
+        }
+
+
+    }
+}
